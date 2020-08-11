c78e630a4868cac9df2c6738a8fdff38bb7cd7c3
Sandip Samal <sandipsamal@pop-os.localdomain>
2020-08-11 13:09:26 -0400

    Added base code

--- mricnn_predict/mricnn_predict.py
+++ mricnn_predict/mricnn_predict.py
@@ -131,6 +131,83 @@ class Mricnn_predict(ChrisApp):
         Define the CLI arguments accepted by this plugin app.
         Use self.add_argument to specify a new app argument.
         """
+    def load_test_data():
+   
+    def post_process_data():
+         
+    def predict(self,options):
+
+
+
+        print('-'*30)
+        print('Loading and preprocessing test data...')
+        print('-'*30)
+        create_test_data(options)
+
+        imgs_test = load_test_data(options)
+        imgs_test = imgs_test.astype('float32')
+
+
+        imgs_test /= 255.  # scale masks to [0, 1]
+
+
+        print('-'*30)
+        print('Loading saved weights...')
+        print('-'*30)
+
+        model = self.get_unet()
+        weight_dir =options.inputdir+ '/weights'
+        if not os.path.exists(weight_dir):
+            os.mkdir(weight_dir)
+        model.load_weights(os.path.join(weight_dir, project_name + '.h5'))
+
+        print('-'*30)
+        print('Predicting masks on test data...')
+        print('-'*30)
+
+        imgs_mask_test = model.predict(imgs_test, batch_size=1, verbose=1)
+
+        npy_mask_dir = 'test_mask_npy'
+        if not os.path.exists(npy_mask_dir):
+            os.mkdir(npy_mask_dir)
+
+        np.save(os.path.join(npy_mask_dir, project_name + '_mask.npy'), imgs_mask_test)
+
+        print('-' * 30)
+        print('Saving predicted masks to files...')
+        print('-' * 30)
+
+        imgs_mask_test = preprocess_squeeze(imgs_mask_test)
+        # imgs_mask_test /= 1.7
+        #imgs_mask_test = np.around(imgs_mask_test, decimals=0)
+        #info = np.iinfo(np.uint16) # Get the information of the incoming image type
+        #imgs_mask_test = imgs_mask_test.astype(np.uint16)
+        #imgs_mask_test=imgs_mask_test* info.max # convert back to original class/labels
+        imgs_mask_test = (imgs_mask_test*255.).astype(np.uint8)
+        count_visualize = 1
+        count_processed = 0
+        pred_dir = 'preds'
+        if not os.path.exists(pred_dir):
+            os.mkdir(pred_dir)
+        pred_dir = os.path.join(options.outputdir, project_name)
+        if not os.path.exists(pred_dir):
+            os.mkdir(pred_dir)
+        for x in range(0, imgs_mask_test.shape[0]):
+            for y in range(0, imgs_mask_test.shape[1]):
+                if (count_visualize > 1) and (count_visualize < 16):
+                    save_img=imgs_mask_test[x][y].astype(np.uint16)
+                    imsave(os.path.join(pred_dir, 'pred_' +str( count_processed )+ '.png'), save_img)
+                    count_processed += 1
+
+                count_visualize += 1
+                if count_visualize == 17:
+                    count_visualize = 1
+                if (count_processed % 100) == 0:
+                    print('Done: {0}/{1} test images'.format(count_processed, imgs_mask_test.shape[0]*14))
+
+        print('-'*30)
+        print('Prediction finished')
+        print('-'*30)
 
     def run(self, options):
         """
